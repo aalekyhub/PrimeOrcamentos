06dbe6b2c4be1795c47ebc61c47b776241469869
Aleky Franco <aaleky@hotmail.com>
2026-01-19 11:18:40 -0300

    Atualizações no PrimeOrcamentos

--- components/WorkOrderManager.tsx
+++ components/WorkOrderManager.tsx
@@ -341,7 +341,7 @@ const WorkOrderManager: React.FC<Props> = ({ orders, setOrders, customers, setCu
                     </div>
                 </div>
                 <div class="text-right">
-                    <div class="bg-blue-600 text-white px-4 py-1 rounded text-[8px] font-black uppercase tracking-widest mb-1 inline-flex items-center justify-center">CONTRATO</div>
+                    <div class="bg-blue-600 text-white px-4 py-1.5 rounded text-[8px] font-black uppercase tracking-widest mb-1 inline-block text-center leading-none">CONTRATO</div>
                     <h2 class="text-3xl font-black text-slate-900 tracking-tighter">${order.id}</h2>
                     <div class="mt-2 space-y-0.5"><p class="text-[8px] font-black text-slate-400 uppercase tracking-widest text-right">EMISSÃO: ${new Date().toLocaleDateString('pt-BR')}</p></div>
                 </div>
